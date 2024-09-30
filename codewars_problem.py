98ff2eb193e1efe92ad3af7db6348f3ef230fb22
Renzo Emmanuel Ramos <143341990+RenzoEmamnuel@users.noreply.github.com>
2024-09-30 22:45:33 +0800

    Update codewars_problem.py

--- codewars_problem.py
+++ codewars_problem.py
@@ -194,6 +194,9 @@ def ml(team):
         return f"You can have 0 teams"
     else:
         return f"You can have {min(dict_team.values())}"
-
+def sum_two_smallest_number(numbers) :
+    return sum(sorted(numbers[:2]))
+def filter_list(l):
+    return [x for x in l if isinstance(x, int)]
 
 
