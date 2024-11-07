d9d987c635b7ce07207d6d022b08615657108d4d
Renzo Emmanuel Ramos <143341990+RenzoEmamnuel@users.noreply.github.com>
2024-11-07 22:20:30 +0800

    Update codewars_problem.py

--- codewars_problem.py
+++ codewars_problem.py
@@ -346,4 +346,4 @@ def hello(name=''):
 def fuck_you_Kristine ():#cheat day # another cheat day fuck it 
     retrun "Im so tired"
     #another cheat day # more cheat day # another #more # more
-    #exam week jusko
+    #exam week jusko # new
