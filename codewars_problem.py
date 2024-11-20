911ead7c931dae4f4076f68f065ba8a390355c4b
Renzo Emmanuel Ramos <143341990+RenzoEmamnuel@users.noreply.github.com>
2024-11-20 21:12:26 +0800

    Update codewars_problem.py

--- codewars_problem.py
+++ codewars_problem.py
@@ -346,7 +346,7 @@ def hello(name=''):
 def fuck_you_Kristine ():#cheat day # another cheat day fuck it 
     retrun "Im so tired"
     #another cheat day # more cheat day # another #more # more
-    #exam week jusko # newb #bro so tired #sobrang busy fuck #now na #sigh #renzo #hi # tan asdsa
+    #exam week jusko # newb #bro so tired #sobrang busy fuck #now na #sigh #renzo #hi # tan asdsaasdsa
 def hex_to_dec(s):
     return int(s,16)
 def sequence_sum(start, end, step):
