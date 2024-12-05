683905eee67b4f1d1873220bc2eff3af22d2201c
Renzo Emmanuel Ramos <143341990+RenzoEmamnuel@users.noreply.github.com>
2024-12-05 21:55:48 +0800

    Update codewars_problem.py

--- codewars_problem.py
+++ codewars_problem.py
@@ -1,4 +1,4 @@
-def replacement ():                                                         #replacement hehehe
+def replacement ():                                                         #replacheement hehehe
     x = "hello world!"
     print(x.replace("!","?"))
 def counterfeit():                                                          # __ counterfeit of a number
